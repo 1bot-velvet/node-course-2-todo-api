6ae08c73d6fbeaea58da0abe8ff8d4a673c3beb4
Only Person <1bot@velvetray.com>
2018-12-30 20:22:49 +0000

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 //useMongoClient: true; Line of code as getting deprecation errors
 mongoose.Promise = global.Promise; //use local Promise library, not a 3rd party one
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
+
+
+//mongodb://heroku_9zm1rxqn:9d4bh3p9ngohp5dj2tjchv3j0c@ds145584.mlab.com:45584/heroku_9zm1rxqn
