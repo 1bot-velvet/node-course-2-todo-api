afbc74c38de377ae84dc51a3615040908f690e05
Only Person <1bot@velvetray.com>
2019-01-04 23:38:45 +0000

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -102,6 +101,21 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+//POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
