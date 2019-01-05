2721567c8c72b716c68ecfaf60f472dede253d10
Only Person <1bot@velvetray.com>
2019-01-05 02:08:11 +0000

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,32 @@
-//const {SHA256} = require('crypto-js');
+const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$T/ndJ.7VogpML5vGw5P91.NGShoftImRKT1yqnH9pHP92MpF6Tsja';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
 
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
+//
+//
 
 
 
