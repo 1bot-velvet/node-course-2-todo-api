73c5a5fd83bf2ec972bb47d2ffe32f87eb087743
Only Person <1bot@velvetray.com>
2019-01-14 16:21:23 +0000

    Add POST /user/login route

--- server/models/user.js
+++ server/models/user.js
@@ -74,6 +74,28 @@ UserSchema.statics.findByToken = function (token) {
   })
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+// Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
+
 //Mongoose middleware which runs only if password has been changed/created and runs before a save to the database
 UserSchema.pre('save', function (next) {
   var user = this;
@@ -103,4 +125,4 @@ module.exports = {User};
 //   console.log('User saved', doc);
 // }, (e) => {
 //   console.log('Unable to save user', e);
-// });
+//
