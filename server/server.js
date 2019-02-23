edd43d8e40c7d263fd76c06dea42cf24732bcfbe
Only Person <1bot@velvetray.com>
2019-02-23 14:33:22 +0000

    promises changed to async (x3)

--- server/server.js
+++ server/server.js
@@ -63,27 +63,26 @@ app.get('/todos/:id', authenticate, (req, res) => {
   });
 });
 
-//DELETE TODOS ROUTE
-app.delete('/todos/:id', authenticate, (req, res) => {
-  //get the id
-  var id = req.params.id;
-  //validate the ID - not valid? return 404
+//DELETE TODOS ROUTE ASYNC
+app.delete('/todos/:id', authenticate, async (req, res) => {
+  const id = req.params.id;
+
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   };
-  //remove todo by id
-  Todo.findOneAndRemove({
-    _id: id,
-    _creator: req.user._id
-  }).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();//doc not found
-    };
-    res.send({todo});//success
-  }).catch((e) => {
-    res.status(400).send();
-  });
 
+  try {
+    const todo = await Todo.findOneAndRemove({
+      _id: id,
+      _creator: req.user._id
+    });
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  } catch (e) {
+    res.status(400).send();
+  }
 });
 
 //PATCH TODOS ROUTE
@@ -116,22 +115,17 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 
 });
 
-//POST /users ROUTE
-app.post('/users', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-  var user = new User(body);
-
-  user.save().then((user) => {
-  //   res.send(user);
-  // }).catch((e) => {
-  //   res.status(400).send(e);
-  // })
-    return user.generateAuthToken();
-  }).then((token) => {
+//POST /users ROUTE ASYNC
+app.post('/users', async (req, res) => {
+  try {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = new User(body);
+    await user.save();
+    const token = await user.generateAuthToken();
     res.header('x-auth', token).send(user);
-  }).catch((e) => {
+  } catch (e) {
     res.status(400).send(e);
- })
+  }
 });
 
 //private route
@@ -139,28 +133,27 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-// POST /users/login {email, password} creating private route for login
-app.post('/users/login', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-
-  User.findByCredentials(body.email, body.password).then((user) => {
-    return user.generateAuthToken().then((token) => {
-      res.header('x-auth', token).send(user);
-    });
-  }).catch((e) => {
-    res.status(400).send();
-  });
+// POST /users/login {email, password} creating private route for login ASYNC
+app.post('/users/login', async (req, res) => {
+  try{
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = await User.findByCredentials(body.email, body.password);
+    const token = await user.generateAuthToken();
+    res.header('x-auth', token).send(user);
+  } catch (e) {
+      res.status(400).send();
+  }
 });
 
-//remove user token (logging out)
-app.delete('/users/me/token', authenticate, (req, res) => {
-  req.user.removeToken(req.token).then(() => {
+//remove user token (logging out) ASYNC
+app.delete('/users/me/token', authenticate, async (req, res) => {
+  try {
+    await req.user.removeToken(req.token);
     res.status(200).send();
-  }, () => {
+  } catch (e) {
     res.status(400).send();
-  });
+  }
 });
-
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
